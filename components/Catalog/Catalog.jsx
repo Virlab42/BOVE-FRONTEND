dfe54f7f75b88c905388322dabc8dbc8bc85c448
neizbejnoezlo <neizbejnoezlo@gmail.com>
2025-11-28 15:23:21 +0700

    tot

--- components/Catalog/Catalog.jsx
+++ components/Catalog/Catalog.jsx
@@ -238,7 +238,7 @@ export default function Catalog({ initialCat }) {
                   ) : (
                     // пустое сердечко
                     <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
-                      <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 16C1 12 2 6 7 4s8 2 9 4c1-2 5-6 10-4s5 8 2 12s-12 12-12 12s-9-8-12-12" />
+                      <path fill="transparent" stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 16C1 12 2 6 7 4s8 2 9 4c1-2 5-6 10-4s5 8 2 12s-12 12-12 12s-9-8-12-12" />
                     </svg>
                   )}
                 </button>
