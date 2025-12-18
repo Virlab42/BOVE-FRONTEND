7ee730a4265020b112be973ebb1bf474f27289f8
id0ntc4re <parsikovevgenij470@gmail.com>
2025-12-18 09:50:28 +0700

    info

--- src/app/info/page.jsx
+++ src/app/info/page.jsx
@@ -3,8 +3,6 @@ import { useState } from "react";
 import "./InfoPage.scss";
 import Image from "next/image";
 
-
-
 export default function InfoPage() {
   const [openSection, setOpenSection] = useState("contacts");
 
@@ -17,7 +15,6 @@ export default function InfoPage() {
       <h1 className="info-page__title">Информация</h1>
 
       <div className="info-page__accordion">
-
         {/* Контакты */}
         <div className="accordion-item">
           <button
@@ -25,19 +22,33 @@ export default function InfoPage() {
             onClick={() => toggleSection("contacts")}
           >
             Контакты
-            <span className={`accordion-icon ${openSection === "contacts" ? "open" : ""}`}>▼</span>
+            <span
+              className={`accordion-icon ${
+                openSection === "contacts" ? "open" : ""
+              }`}
+            >
+              ▼
+            </span>
           </button>
           <div
-            className={`accordion-body ${openSection === "contacts" ? "open" : ""}`}
+            className={`accordion-body ${
+              openSection === "contacts" ? "open" : ""
+            }`}
           >
             <div className="contacts-item">
               Телефон: <a href="tel:+7 (996) 415-72-30">+7 (996) 415-72-30</a>
             </div>
             <div className="contacts-item">
-              Email: <a href="mailto:radostev.alexandr42@yandex.ru">radostev.alexandr42@yandex.ru</a>
+              Email:{" "}
+              <a href="mailto:radostev.alexandr42@yandex.ru">
+                radostev.alexandr42@yandex.ru
+              </a>
             </div>
             <div className="contacts-item">
-              Telegram: <a href="https://t.me/@alsergeevich" target="_blank">@alsergeevich</a>
+              Telegram:{" "}
+              <a href="https://t.me/@alsergeevich" target="_blank">
+                @alsergeevich
+              </a>
             </div>
           </div>
         </div>
@@ -49,18 +60,56 @@ export default function InfoPage() {
             onClick={() => toggleSection("documents")}
           >
             Документы
-            <span className={`accordion-icon ${openSection === "documents" ? "open" : ""}`}>▼</span>
+            <span
+              className={`accordion-icon ${
+                openSection === "documents" ? "open" : ""
+              }`}
+            >
+              ▼
+            </span>
           </button>
           <div
-            className={`accordion-body ${openSection === "documents" ? "open" : ""}`}
+            className={`accordion-body ${
+              openSection === "documents" ? "open" : ""
+            }`}
           >
             <ul>
-              <li><a href="/docs/политика_конфиденциальности.pdf" target="_blank">Политика конфиденциальности</a></li>
-              <li><a href="/docs/согласие_на_обработку_пдн.pdf" target="_blank">Согласие на обработку ПДН</a></li>
-              <li><a href="/docs/согласие_на_обработку_пдн_в_целях_рассылки.pdf" target="_blank">Согласие на обработку ПДН в целях рассылки</a></li>
-              <li><a href="/docs/согласие_на_передачу_пдн.pdf" target="_blank">Согласие на передачу ПДН</a></li>
-              <li><a href="/docs/согласие_на_получение_рекламы.pdf" target="_blank">Согласие на получение рекламы</a></li>
-              <li><a href="/docs/публичная_оферта.pdf" target="_blank">Публичная оферта</a></li>
+              <li>
+                <a href="/docs/политика_конфиденциальности.pdf" target="_blank">
+                  Политика конфиденциальности
+                </a>
+              </li>
+              <li>
+                <a href="/docs/согласие_на_обработку_пдн.pdf" target="_blank">
+                  Согласие на обработку ПДН
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/docs/согласие_на_обработку_пдн_в_целях_рассылки.pdf"
+                  target="_blank"
+                >
+                  Согласие на обработку ПДН в целях рассылки
+                </a>
+              </li>
+              <li>
+                <a href="/docs/согласие_на_передачу_пдн.pdf" target="_blank">
+                  Согласие на передачу ПДН
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/docs/согласие_на_получение_рекламы.pdf"
+                  target="_blank"
+                >
+                  Согласие на получение рекламы
+                </a>
+              </li>
+              <li>
+                <a href="/docs/публичная_оферта.pdf" target="_blank">
+                  Публичная оферта
+                </a>
+              </li>
             </ul>
           </div>
         </div>
@@ -72,15 +121,90 @@ export default function InfoPage() {
             onClick={() => toggleSection("stores")}
           >
             Наши магазины
-            <span className={`accordion-icon ${openSection === "stores" ? "open" : ""}`}>▼</span>
+            <span
+              className={`accordion-icon ${
+                openSection === "stores" ? "open" : ""
+              }`}
+            >
+              ▼
+            </span>
           </button>
           <div
-            className={`accordion-body ${openSection === "stores" ? "open" : ""}`}
+            className={`accordion-body ${
+              openSection === "stores" ? "open" : ""
+            }`}
           >
             <p>Скоро здесь появятся магазины</p>
           </div>
         </div>
 
+        <div className="accordion-item">
+          <button
+            className="accordion-header"
+            onClick={() => toggleSection("stores")}
+          >
+            Доставка
+            <span
+              className={`accordion-icon ${
+                openSection === "stores" ? "open" : ""
+              }`}
+            >
+              ▼
+            </span>
+          </button>
+          <div
+            className={`accordion-body ${
+              openSection === "stores" ? "open" : ""
+            }`}
+          >
+            <p>
+              Доставка осуществляется до пункта выдачи в вашем городе. Стоимость
+              доставки через службу СДЭК рассчитывается индивидуально. Срок
+              доставки 1-6 дней.
+            </p>
+            <p>
+              Доставка по Москве в пределах МКАД 800₽. За пределами МКАД,
+              рассчитывается индивидуально.
+            </p>
+            <p>
+              Доставка осуществляется компанией СДЭК. Если коробка пришла в
+              непрезентабельном виде (помятая, разорванная), то пожалуйста
+              свяжитесь с нами в пункте выдачи.
+            </p>
+          </div>
+        </div>
+
+        <div className="accordion-item">
+          <button
+            className="accordion-header"
+            onClick={() => toggleSection("stores")}
+          >
+            Возврат
+            <span
+              className={`accordion-icon ${
+                openSection === "stores" ? "open" : ""
+              }`}
+            >
+              ▼
+            </span>
+          </button>
+          <div
+            className={`accordion-body ${
+              openSection === "stores" ? "open" : ""
+            }`}
+          >
+            <p>
+              Вы можете оформить возврат в течение 7 дней с момента получения
+              заказа при условии сохранения его товарного вида: без следов
+              эксплуатации, с заводскими бирками, в первоначальной упаковке
+              (Федеральный закон о защите прав потребителей, ст.26.1)
+            </p>
+            <p>
+              Обратите внимание: товар бывший в употреблении, не подлежит
+              возврату.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
