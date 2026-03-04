1ab0d1ea5873216097127b54ac3c65ae78ba6dc0
Ser0jka123 <sersur42@yandex.ru>
2026-03-04 14:10:47 +0700

    upd

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -120,6 +120,9 @@ function Footer() {
             </div>
           </div>
         </div>
+        <div className="footer_copyright">
+            <a href="https://virlab42.ru">Сайт разработан компанией <span>Вирлаб</span></a>
+          </div>
       </footer>
     </>
   );
